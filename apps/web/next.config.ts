6a85ebf57985c06d090c44dd915cadda6c5123f2
TB2MOY <tb2moy@gmail.com>
2026-04-08 20:23:24 +0300

    feat: add next.config.ts

---
+++ apps/web/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  transpilePackages: ['maplibre-gl'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'mapbox-gl': 'maplibre-gl',
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
